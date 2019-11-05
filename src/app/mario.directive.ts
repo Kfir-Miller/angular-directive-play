b2704f6ffd944a9293b132dd9beb6afac845aa0f
Kfir-Miller <kfir@mie.co.il>
2019-11-05 14:40:06 +0200

    3 files modified

--- src/app/mario.directive.ts
+++ src/app/mario.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[mario]'
@@ -6,14 +6,48 @@ import { Directive, HostListener } from '@angular/core';
 export class MarioDirective {
   private sound;
 
-  constructor() { }
+  private marioData = {
+    song: 'https://www.myinstants.com/media/sounds/untitled_3.mp3',
+    image: 'https://upload.wikimedia.org/wikipedia/he/a/a9/MarioNSMBUDeluxe.png',
+    color: '#E52521'
+  };
+
+
+  constructor(private el: ElementRef) {
+    this.sound = new Audio(this.marioData.song);
+  }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.sound = new Audio('https://www.myinstants.com/media/sounds/untitled_3.mp3').play();
+    this.sound.play();
+    this.addImage();
+    this.changeColor(this.marioData.color);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.sound.pause();
     this.sound.currentTime = 0;
+    this.removeImage();
+    this.changeColor(null);
   }
+
+  private addImage() {
+    let img = document.createElement("img");
+    const el = this.el.nativeElement;
+    img.id = 'mario-img';
+    img.src = this.marioData.image;
+    img.style.cssText = `width: 100px;
+                        display: block`;
+    el.parentNode.insertBefore(img, el.nextSibling);
+  }
+
+
+  private removeImage() {
+    document.getElementById("mario-img").remove();
+  }
+
+  private changeColor(color) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+  
+
 }
